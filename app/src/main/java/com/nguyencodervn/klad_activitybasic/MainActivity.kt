41abc4b4bbc8de280dcbc39e9f856a045b3db2fc
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-11-08 10:57:45 +0700

    start code

--- app/src/main/java/com/nguyencodervn/klad_activitybasic/MainActivity.kt
+++ app/src/main/java/com/nguyencodervn/klad_activitybasic/MainActivity.kt
@@ -6,20 +6,14 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainToABt : Button
-    private lateinit var mainToBBt : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mainToABt = findViewById(R.id.mainToABt)
-        mainToBBt = findViewById(R.id.mainToBBt)
 
         mainToABt.setOnClickListener {
             // Tạo câu lệnh chuyển sang Activity A
-        }
-
-        mainToABt.setOnClickListener {
-            // Tạo câu lệnh chuyển sang Activity B
         }
     }
 }
