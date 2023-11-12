f1ab7993efb3dae163148d07d56e051bdf3dc98a
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-11-12 10:27:43 +0700

    Activity Lifecycle Start

--- app/src/main/java/com/nguyencodervn/klad_activitybasic/MainActivity.kt
+++ app/src/main/java/com/nguyencodervn/klad_activitybasic/MainActivity.kt
@@ -4,20 +4,38 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainToABt : Button
+    private lateinit var countBt : Button
+    private lateinit var finishBt : Button
+    private lateinit var countTv : TextView
+        private var count : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mainToABt = findViewById(R.id.mainToABt)
+        countBt = findViewById(R.id.countBt)
+        finishBt = findViewById(R.id.finishBt)
+        countTv = findViewById(R.id.countTv)
+        countTv.text = "0"
 
         mainToABt.setOnClickListener {
             val intent = Intent(this, ActivityA::class.java)
             startActivity(intent)
+        }
+
+        finishBt.setOnClickListener {
+            finish()
+        }
+
+        countBt.setOnClickListener {
+            count += 1
+            countTv.text = count.toString()
         }
     }
 }
