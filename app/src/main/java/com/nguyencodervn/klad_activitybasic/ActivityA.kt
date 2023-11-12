2ee11260a59b4f09f5b07c5af83a3e9f54763081
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-11-12 12:36:26 +0700

    Activity Lifecycle Start

--- app/src/main/java/com/nguyencodervn/klad_activitybasic/ActivityA.kt
+++ app/src/main/java/com/nguyencodervn/klad_activitybasic/ActivityA.kt
@@ -2,7 +2,6 @@ package com.nguyencodervn.klad_activitybasic
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import timber.log.Timber
 
 class ActivityA : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/nguyencodervn/klad_activitybasic/MainActivity.kt
+++ app/src/main/java/com/nguyencodervn/klad_activitybasic/MainActivity.kt
@@ -1,27 +1,39 @@
 package com.nguyencodervn.klad_activitybasic
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainToABt: Button
     private lateinit var countBt: Button
     private lateinit var finishBt: Button
     private lateinit var countTv: TextView
+    private lateinit var mainTv: TextView
     private var count: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initId()
+        setupEven()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initId() {
         mainToABt = findViewById(R.id.mainToABt)
         countBt = findViewById(R.id.countBt)
         finishBt = findViewById(R.id.finishBt)
         countTv = findViewById(R.id.countTv)
+        mainTv = findViewById(R.id.mainTv)
         countTv.text = "0"
-        setupEven()
+        val api = Build.VERSION.SDK_INT
+        mainTv.text = "MainActivity run API $api"
     }
 
     private fun setupEven() {
