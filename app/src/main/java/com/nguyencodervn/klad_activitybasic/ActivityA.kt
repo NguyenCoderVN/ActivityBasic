6ba9de85dea6bc68226f9a38867b5a9c293cabc7
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-11-19 13:58:29 +0700

    Transfer Data between Activities Start

--- app/src/main/java/com/nguyencodervn/klad_activitybasic/ActivityA.kt
+++ app/src/main/java/com/nguyencodervn/klad_activitybasic/ActivityA.kt
@@ -12,36 +12,12 @@ class ActivityA : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a)
-        Timber.i("onCreate")
+
         receivedTv = findViewById(R.id.receivedTv)
         returnBt = findViewById(R.id.returnBt)
+
         returnBt.setOnClickListener {
             finish()
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Timber.i("onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Timber.i("onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Timber.i("onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Timber.i("onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Timber.i("onDestroy")
     }
 }
--- app/src/main/java/com/nguyencodervn/klad_activitybasic/MainActivity.kt
+++ app/src/main/java/com/nguyencodervn/klad_activitybasic/MainActivity.kt
@@ -7,12 +7,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
-import timber.log.Timber
 
-class MainActivity : AppCompatActivity(), LifecycleEventObserver {
+class MainActivity : AppCompatActivity() {
     private lateinit var mainToABt: Button
     private lateinit var countBt: Button
     private lateinit var countTv: TextView
@@ -22,37 +18,9 @@ class MainActivity : AppCompatActivity(), LifecycleEventObserver {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lifecycle.addObserver(this)
+
         initId()
         setupEven()
-    }
-
-    companion object {
-        const val KEY_COUNT = "KEY_COUNT"
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt(KEY_COUNT, count)
-        Timber.i("onSaveInstanceState")
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        count = savedInstanceState.getInt(KEY_COUNT)
-        countTv.text = count.toString()
-        Timber.i("onRestoreInstanceState")
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun initId() {
-        mainToABt = findViewById(R.id.mainToABt)
-        countBt = findViewById(R.id.countBt)
-        countTv = findViewById(R.id.countTv)
-        mainTv = findViewById(R.id.mainTv)
-        countTv.text = "0"
-        val api = Build.VERSION.SDK_INT
-        mainTv.text = "MainActivity run API $api"
     }
 
     private fun setupEven() {
@@ -67,15 +35,29 @@ class MainActivity : AppCompatActivity(), LifecycleEventObserver {
         }
     }
 
-    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        when (event) {
-            Lifecycle.Event.ON_STOP -> Timber.i("onStop");
-            Lifecycle.Event.ON_CREATE -> Timber.i("onCreate");
-            Lifecycle.Event.ON_START -> Timber.i("onStart");
-            Lifecycle.Event.ON_RESUME -> Timber.i("onResume")
-            Lifecycle.Event.ON_PAUSE -> Timber.i("onPause");
-            Lifecycle.Event.ON_DESTROY -> Timber.i("onDestroy");
-            Lifecycle.Event.ON_ANY -> Timber.i("onAny");
-        }
+    companion object {
+        const val KEY_COUNT = "KEY_COUNT"
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initId() {
+        mainToABt = findViewById(R.id.mainToABt)
+        countBt = findViewById(R.id.countBt)
+        countTv = findViewById(R.id.countTv)
+        mainTv = findViewById(R.id.mainTv)
+        countTv.text = "0"
+        val api = Build.VERSION.SDK_INT
+        mainTv.text = "MainActivity run API $api"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(KEY_COUNT, count)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getInt(KEY_COUNT)
+        countTv.text = count.toString()
     }
 }
